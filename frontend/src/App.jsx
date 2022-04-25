69c8781fdbaeb0d484d9095e42add58e8b3b8c1e
FedkinAndrey <fedkin17@gmail.com>
2022-04-25 22:38:05 +0300

    added navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css';
+import './App.scss';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { Auth, Events, Bookings } from './pages';
+import { MainNavigation } from './components';
 
 function App() {
   return (
-    <Routes>
-      <>
-        <Route path="/" element={<Navigate replace to="/auth" />} exact />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/bookings" element={<Bookings />} />
-      </>
-    </Routes>
+    <>
+      <MainNavigation />
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/auth" />} exact />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/bookings" element={<Bookings />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
