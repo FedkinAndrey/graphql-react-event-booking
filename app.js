0d57dad64fd6f84f27fabea7ba6200af9bc734ab
FedkinAndrey <fedkin17@gmail.com>
2022-04-26 17:32:59 +0300

    react auth. hitting api

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ import { isAuthMiddleware } from './middleware/is-auth.js';
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuthMiddleware);
 
 app.use(
@@ -25,8 +35,8 @@ mongoose
     `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.mqrxt.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(3000, () => {
-      console.log('listen port 3000');
+    app.listen(8000, () => {
+      console.log('listen port 8000');
     });
   })
   .catch((err) => {
--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,10 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './auth.scss';
 
 const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogin, setIsLogin] = useState(true);
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onChangeLogin = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+        query { 
+          login(email: "${email}", password: "${password}") { 
+            userId
+            token
+            tokenExpiration 
+          } 
+        }
+      `,
+    };
+
+    if (!isLogin) {
+      requestBody = {
+        query: `mutation { createUser(userInput: {email: "${email}", password: "${password}"}) { _id email } }`,
+      };
+    }
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed');
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        console.log(resData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div>
-      <h1>Auth page</h1>
-    </div>
+    <form className="auth-form" onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="email">E-Mail</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => onChangeEmail(e)}
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => onChangePassword(e)}
+        />
+      </div>
+      <div className="form-actions">
+        <button type="submit">Submit</button>
+        <button type="button" onClick={onChangeLogin}>
+          Switch to {isLogin ? 'SignUp' : 'Login'}
+        </button>
+      </div>
+    </form>
   );
 };
 
